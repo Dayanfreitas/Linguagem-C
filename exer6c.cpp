bbbe3d3df1fbec816c973fb9854948d094b583ea
israelmurilocadorin <37721806+israelmurilocadorin@users.noreply.github.com>
2018-04-22 18:15:09 -0300

    Update exer6c.cpp

--- exer6c.cpp
+++ exer6c.cpp
@@ -4,35 +4,35 @@
 #include <ctype.h>
 #include <locale.h>
 
-int main (){
+int main(){
     int i;
     int x1;
     int x2;
     int soma;
     setlocale (LC_ALL, "");
     
-	printf ("Digite o primeiro número(deve ser menor que o segundo): ");
-    scanf ("%d", &x1);
-    printf ("\n");
+	printf("Digite o primeiro nÃºmero(deve ser menor que o segundo): ");
+    scanf("%d", &x1);
+    printf("\n");
     
-	printf ("Digite o segundo número: ");
-    scanf ("%d", &x2);
-    printf ("\n");
-    if (x2 > x1){
+	printf("Digite o segundo nÃºmero: ");
+    scanf("%d", &x2);
+    printf("\n");
+    if(x2 > x1){
        soma = 0;
        
-       for (i = x1; i <= x2; i++){
+       for(i = x1; i <= x2; i++){
            soma = x1 + soma;
            x1++;
        }
-       printf ("Soma: %d\n", soma);
+       printf("Soma: %d\n", soma);
     }
     else {
-         printf ("Os números não são válidos\n");
+         printf("Os nÃºmeros nÃ£o sÃ£o vÃ¡lidos\n");
     }     
     
     printf("\n");
-	system ("pause");
+	system("pause");
     return 0;
 }
 
