5e052d7569ca97221ae7c022c486cb98a47ab9da
israelmurilocadorin <37721806+israelmurilocadorin@users.noreply.github.com>
2018-04-22 22:40:11 -0300

    Add files via upload

---
+++ exer11C.cpp
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+
+int main (){
+    
+	int i, j;
+    int resultado[11];
+    
+    for (i = 1; i <= 11; i++){
+        
+        for (j = 1; j <= 11; j++){
+            if(j <= 10){
+			resultado [j] = i * j;
+            printf("%d * %d = %d\n", i, j, resultado[j]);
+        	}
+        	else{
+        		break;
+			}
+		}
+    printf("\n");
+    }
+    
+	system("pause");
+    return 0;
+}
+
